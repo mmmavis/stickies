c8c48f4c37792db142ff1fe7f2433d9e030b1629
Mavis Ou <mmmavis@users.noreply.github.com>
2018-03-26 14:27:09 -0700

    yay sticky wall

---
+++ script.js
@@ -0,0 +1,109 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDwy5Kbmd7GHTdXbRewd8OZaArBWPKDurs",
+  authDomain: "erika-stickies.firebaseapp.com",
+  databaseURL: "https://erika-stickies.firebaseio.com",
+  projectId: "erika-stickies",
+  storageBucket: "erika-stickies.appspot.com",
+  messagingSenderId: "282840178411"
+};
+
+firebase.initializeApp(config);
+
+firebase.auth().signInAnonymously().catch(function(error) {
+  console.log("error", error);
+});
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    // var isAnonymous = user.isAnonymous;
+    // var uid = user.uid;
+    // console.log("user");
+
+    renderAllStickies(function() {
+      
+    });
+  } else {
+    // User is signed out.
+    // console.log("out");
+  }
+});
+
+Handlebars.registerHelper("localTime", function(timestamp) {
+  return moment.unix(timestamp/1000).format("YYYY-MM-DD hh:mm a");
+});
+
+
+function renderAllStickies(done) {
+  firebase.database().ref('/stickies').once('value').then(function(snapshot) {
+    var stickies = snapshot.val();
+
+    Object.keys(stickies).forEach(function(key) {
+      var sticky = stickies[key];
+      sticky.id = key;
+      console.log("sticky", sticky);
+      renderSticky(sticky);
+    })
+
+    done();
+  });
+}
+
+function createStickyHTML(sticky) {
+  var source = document.getElementById("sticky-template").innerHTML;
+  var template = Handlebars.compile(source);
+
+  return template(sticky);
+}
+
+function isSrcVideo(filename = "") {
+  var ext = filename.split("?")[0].split(".").pop();
+
+  switch (ext.toLowerCase()) {
+    case "mp4":
+    case "avi":
+    case "mpg":
+      return true;
+    }
+
+  return false;
+}
+
+function renderSticky(sticky) {
+  // check if imgSrc is actually a video...
+  if (isSrcVideo(sticky.imgSrc)) {
+    sticky.imgSrcIsVideo = true;
+  }
+
+  $("#wall").append(createStickyHTML(sticky));
+}
+
+// send new sticky
+$("body").on("submit", "#new-sticky-form", function(event) {
+  event.preventDefault();
+
+  var ref = firebase.database().ref("stickies").push();
+  var task = ref.set({
+    timestamp: Date.now().toString(),
+    message: $(this).find("[name=message]").val().replace(/\n/g,"<br>"),
+    submitter: $(this).find("[name=submitter]").val(),
+    imgSrc: $(this).find("[name=imgSrc]").val(),
+    bgColor: $(this).find("[name=bgColor]:checked").val(),
+    textColor: null
+  }, function complete() {
+      // reload page so new sticky shows... a cheat...
+      window.location.reload(true);
+      done();
+    }
+  );
+});
+
+$("#new-sticky-form").hide();
+
+$("#pencil-toggle").on("click", function(event) {
+  $("body").toggleClass("show-form");
+  $("#new-sticky-form")[0].reset();
+  $("#new-sticky-form").show();
+  $(this).hide();
+});
